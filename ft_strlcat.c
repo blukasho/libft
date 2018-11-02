77b5a6d3f9a2f58d87cb206bc77cb13c86657fbf
blukasho <bodik1w@gmail.com>
2018-11-02 18:08:10 +0200

    mod Makefile, mod ft_strlcat.c, mod ft_strlen.c, mod libft.h

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                                            */
-/*   ft_strlcat.c                                                             */
-/*                                                                            */
-/*   By: blukasho <bodik1w@gmail.com>                                         */
-/*                                                                            */
-/*   Created: 2018/11/02 11:23:22 by blukasho                                 */
-/*   Updated: 2018/11/02 11:33:01 by blukasho                                 */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/02 16:18:59 by blukasho          #+#    #+#             */
+/*   Updated: 2018/11/02 18:07:02 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t dest_len;
-	size_t i;
+	size_t	d;
+	size_t	s;
 
-	dest_len = ft_strlen(dest);
-	i = 0;
-	
+	s = 0;
+	d = strlen(dest);
+	while (d < size - 1 && src[s])
+		dest[d++] = src[s++];
+	dest[d] = '\0';
+	return ((--d) + strlen(src));
 }
