f80b7b8ea782ee9eeebca9c711dba996e8213e05
blukasho <bodik1w@gmail.com>
2018-11-05 08:53:02 +0200

    mod ft_strncpy.c

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <bodik1w@gmail.com>                                         */
 /*                                                                            */
 /*   Created: 2018/11/02 08:52:15 by blukasho                                 */
-/*   Updated: 2018/11/02 09:08:30 by blukasho                                 */
+/*   Updated: 2018/11/05 08:52:19 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,14 @@
 char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
 	size_t i;
-
+	
 	i = 0;
-	while (i < n && src[i] != '\0')
+	while (src[i] && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	while (i < n)
-	{
-		dest[i] = src[i];	
-		i++;
-	}
+		dest[i++] = '\0';
 	return (dest);
 }
