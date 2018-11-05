b6bd90319578cd5972ce3537087e138282de9155
blukasho <bodik1w@gmail.com>
2018-11-05 20:15:16 +0200

    mod fuck ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,21 +6,28 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/02 16:18:59 by blukasho          #+#    #+#             */
-/*   Updated: 2018/11/02 18:07:02 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/11/05 19:55:52 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t destsize)
 {
 	size_t	d;
 	size_t	s;
 
 	s = 0;
-	d = strlen(dest);
-	while (d < size - 1 && src[s])
-		dest[d++] = src[s++];
-	dest[d] = '\0';
-	return ((--d) + strlen(src));
+	d = strlen(dst);
+	if (destsize)
+	{
+		while (d < destsize - 1 && src[s])
+			dst[d++] = src[s++];
+		dst[d] = '\0';
+		if (d >  destsize)
+			return (strlen(src) + destsize);
+		else
+			return (strlen(src));
+	}
+	return (ft_strlen(src) + (--d));
 }
