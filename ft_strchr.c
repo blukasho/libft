bbbcbc9c0cb5aaec5fb9f7f496ec4b2505636b6f
blukasho <bodik1w@gmail.com>
2018-11-06 09:28:01 +0200

    mod ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/02 18:41:21 by blukasho          #+#    #+#             */
-/*   Updated: 2018/11/02 19:34:11 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/11/06 09:27:17 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,10 @@
 
 char	*ft_strchr(const char *src, int c)
 {
-	const char	*s;
-
-	s = src;
-	while (s++)
-		if (*s == (char) c)
-			return ((char *) s);
-	if (*s == (char) c)
-		return ((char *) s);
+	while (*src)
+		if (*src++ == (char) c)
+			return ((char *) --src);
+	if (*src == (char) c)
+		return ((char *) src);
 	return (NULL);
 }
