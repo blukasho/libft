08a389a7ac00cb067b97fa163148b735c3cbeb9a
blukasho <bodik1w@gmail.com>
2018-11-06 17:05:19 +0200

    mod ft_strclr.c, mod ft_strnew.c

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/04 10:25:18 by blukasho          #+#    #+#             */
-/*   Updated: 2018/11/06 16:46:06 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/11/06 16:59:17 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strnew(size_t size)
 	{
 		str = (char *)ft_memalloc(++size);
 		if (str)
-			str[size] = '\0';
+			ft_memset(str, '\0', size);
 	}
 	return (str);
 }
