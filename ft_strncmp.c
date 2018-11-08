4e1a6e1ea9588cd634e579c8fcfc28749020d9e3
blukasho <bodik1w@gmail.com>
2018-11-08 11:58:15 +0200

    mod ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/03 15:16:17 by blukasho          #+#    #+#             */
-/*   Updated: 2018/11/07 16:51:25 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/11/08 11:53:09 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	while (s1[i] && (s1[i] == s2[i]) && i < n)
+	while (i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
 }
