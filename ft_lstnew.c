7c57fcfa20ed8a32d1dfa7906d9d4eb755e73abc
blukasho <bodik1w@gmail.com>
2018-11-08 07:57:45 +0200

    mod ft_lstnew.c

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 18:33:21 by blukasho          #+#    #+#             */
-/*   Updated: 2018/11/07 20:13:40 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/11/08 07:56:57 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,20 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*res;
 
-	if (content)
+	if (!(res = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+	if (!content)
 	{
-		res = (t_list *)malloc(sizeof(t_list));
-		if (res)
-		{
-			if (!content)
-			{
-				res->content = NULL;
-				res->content_size = 0;
-				return (res);
-			}
-			res->content = malloc(content_size);
-			if (res->content)
-			{
-				res->content = ft_memcpy(res->content, content, content_size);
-				res->content_size = content_size;
-				res->next = NULL;
-				return (res);
-			}
-		}
+		res->content = NULL;
+		res->content_size = 0;
 	}
-	return (NULL);
+	else
+	{
+		if (!(res->content = malloc(content_size)))
+			return (NULL);
+		res->content = ft_memcpy(res->content, content, content_size);
+		res->content_size = content_size;
+	}
+	
+	return (res);
 }
