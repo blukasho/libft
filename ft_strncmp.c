5afdfab09103afd6fbea470ccc19bd15a6d60c47
blukasho <bodik1w@gmail.com>
2018-11-09 12:45:23 +0200

    mod ft_strncmp.c, mod ft_strrchr.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/03 15:16:17 by blukasho          #+#    #+#             */
-/*   Updated: 2018/11/08 11:53:09 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/11/09 12:43:37 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
-
-	i = 0;
-	while (i < n)
+	while (n-- > 0)
 	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
+		if ((unsigned char)*s1 != (unsigned char)*s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		++s1;
+		++s2;
 	}
 	return (0);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/03 09:48:50 by blukasho          #+#    #+#             */
-/*   Updated: 2018/11/08 13:13:31 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/11/09 12:10:23 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,14 @@ char	*ft_strrchr(const char *s, int c)
 {
 	const char *tmp;
 
-	if (s)
+	tmp = s;
+	while (*s)
+		++s;
+	while (s >= tmp)
 	{
-		tmp = s;
-		while (*s)
-			++s;
-		while (s >= tmp)
-		{
-			if (*s == c)
-				return ((char *)s);
-			--s;
-		}
+		if (*s == c)
+			return ((char *)s);
+		--s;
 	}
 	return (NULL);
 }
