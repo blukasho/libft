07d2ccafff1600df82a35a163fcee47fe61009c9
blukasho <bodik1w@gmail.com>
2018-11-09 12:53:45 +0200

    mod ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/03 15:16:17 by blukasho          #+#    #+#             */
-/*   Updated: 2018/11/09 12:43:37 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/11/09 12:51:33 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	{
 		if ((unsigned char)*s1 != (unsigned char)*s2)
 			return ((unsigned char)*s1 - (unsigned char)*s2);
+		if ((unsigned char)*s1 == '\0')
+			return (0);
 		++s1;
 		++s2;
 	}
